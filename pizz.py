c372f79c1122dc4c835520b000b3e6cff8ab32c3
Peter Gemmell <74552992+Peter-Gemmell@users.noreply.github.com>
2022-04-19 13:49:30 -0400

    Changed

--- pizz.py
+++ pizz.py
@@ -4,22 +4,28 @@
 # Created on March 2022
 # This program calculates the circumference of a circle using constants
 
-import constants
-
-
 def main():
-    # this function calculates the cost of pizza
+    # this function calculates square exponents
+    answer = 0
+    number_squared = 0
 
     # input
-    diameter = int(input("Enter the diameter of the pizza you would like (inch): "))
+    integer_input_string = input("Enter a positive integer: ")
 
-    # process
-    sub_total = constants.LABOR + constants.RENT + (diameter * constants.COST_PER_INCH)
-    total = sub_total + (sub_total * constants.HST)
+    # process & output
+    try:
+        integer_input = int(integer_input_string)
 
-    # output
-    print("")
-    print("The cost of a {0} inch pizza is ${1:,.2f}".format(diameter, total))
+        if integer_input >= 0:
+            for number_squared in range(integer_input + 1):
+                answer = number_squared * number_squared
+                print("{0}² = {1}".format(number_squared, answer))
+        else:
+            print("Sorry, this program only accepts positive integers.")
+
+    except Exception:
+        print("Sorry, that is not a valid integer.")
+
     print("\nDone.")
 
 
